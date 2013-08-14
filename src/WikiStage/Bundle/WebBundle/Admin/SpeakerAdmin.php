7447e7dea3f22853bb4a81bc8917fe182503505e
Romaric Drigon <romaric.drigon@gmail.com>
2013-08-14 22:17:40 +0200

    Added Speaker and Language admin classes

---
+++ src/WikiStage/Bundle/WebBundle/Admin/SpeakerAdmin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace WikiStage\Bundle\WebBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class SpeakerAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+        ;
+    }
+}
