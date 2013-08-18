275647a0e8e8f680caba3aac3d37e3cd20e72386
Romaric Drigon <romaric.drigon@gmail.com>
2013-08-18 20:11:52 +0200

    Added User entity

---
+++ src/WikiStage/Bundle/CoreBundle/Entity/User.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace WikiStage\Bundle\CoreBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string username
+     */
+    private $username;
+
+    /**
+     * @var string e-mail address
+     */
+    private $email;
+
+    /**
+     * @var string salt
+     */
+    private $salt;
+
+    /**
+     * @var string hashed password
+     */
+    private $password;
+
+    /**
+     * @var bool if is active or not
+     */
+    private $isActive;
+
+    public function __construct()
+    {
+        $this->isActive = true;
+        $this->salt = md5(uniqid(null, true));
+    }
+
+    /**
+     * @return int id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return array|\Symfony\Component\Security\Core\User\Role[]
+     */
+    public function getRoles()
+    {
+        return array('ROLE_ADMIN');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @see \Serializable::serialize()
+     */
+    public function serialize()
+    {
+        return serialize(array(
+                $this->id,
+            ));
+    }
+
+    /**
+     * @see \Serializable::unserialize()
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            ) = unserialize($serialized);
+    }
+}
